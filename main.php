2d06e889dbfa23cc9e09aab7a050482ad77833c5
pogi2005 <paulsantiago296@gmail.com>
2025-08-13 02:22:34 +0000

    UnderstandingTheFunctions

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Paul Santiago";
+$age = "19";
+$school = "Global Recipcoral Colleges";
+$course = "BSIT";
+$subject = "WebSys";
+$student = "Yes";
+echo "Name:" . $name . "<br>";
+echo "Age:" . $age . "<br>";
+echo "School:" . $school . "<br>";
+echo "Course:" . $course . "<br>";
+echo "Subject:" . $subject . "<br>";
+echo "Student:" .var_dump($student) . "<br>";
+?>
